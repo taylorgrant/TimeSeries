a05389fe61bd6ded9a12aa4866e6401c84c66204
taylorgrant <taylorgrant38@gmail.com>
2015-10-21 16:41:52 -0700

    Create arfimaSim.R

---
+++ arfimaSim.R
@@ -0,0 +1,27 @@
+```
+arfimaSim <- function(n, d, p=NULL, q=NULL) {
+  # creates a fractionally integrated (p,d,q) process of length n (extends to (1,d,1))
+  # d: fractional differencing parameter (e.g. d=.4, produces series that FI(.4)
+  # p: value of AR parameter
+  # q: value of MA
+  #
+  x <- rnorm(n)
+  d <- -d
+  n <- length(x)
+  k <- seq(1:(n-1))
+  b <- (k-d-1)/k 
+  b <- c(1,cumprod(b))
+  if (is.null(p) && is.null(q)) {
+    df <- filter(b,1,x)
+  } else if (!is.null(p) && is.null(q)) {
+    df <- filter(b,1,x)
+    arx <- filter(1,c(1,-p), df)
+  } else if (is.null(p) && !is.null(q)) {
+    df <- filter(b,1,x)
+    max <- filter(c(1,q), 1, df)
+  } else if (!is.null(p) && !is.null(q)) {
+    df <- filter(b, 1, x)
+    marx <- filter(c(1,q),c(1,-p), df)
+  }
+}
+```
